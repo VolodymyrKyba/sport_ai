d0f9b7eb66335debf9c954e68c5047a82fd4765e
VolodymyrKyba <kybavolodymyr@gmail.com>
2025-02-20 12:43:42 +0000

    4

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel,Field
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn  # Import Uvicorn
+from config import client
 
 app = FastAPI()
 
@@ -15,12 +16,48 @@ app.add_middleware(
 class UserInput(BaseModel):
     name: str = Field(..., example="Johsdn")
 
+def get_team_info(team):
+
+        chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful sport assistant."
+            },
+            {
+                "role": "user",
+                "content": f"""Give me short information about this team {team} in this format:
+                - Current standings
+                - Latest News
+                - Talking Points
+                - Fun Facts
+                - Celebrity Gossip
+                - Workplace Drama
+                - Funny Metaphors
+                """,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+        team_desc = chat_completion.choices[0].message.content
+
+        return team_desc
+
+
+
+
+
 @app.post("/submit")
 async def receive_name(user: UserInput):
-    n = len(user.name)
-    
 
-    return {"message": n}
+    team = user.name
+    
+    
+    
+    text = str(get_team_info(team))
+
+    return {"message": text}
 
 # print(receive_name())
 # Run Uvicorn when this script is executed directly

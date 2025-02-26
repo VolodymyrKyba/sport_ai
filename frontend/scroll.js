eba7e871a409bf6b96670f6b4d2eeddc311a15a4
VolodymyrKyba <kybavolodymyr@gmail.com>
2025-02-26 15:44:25 +0000

    7

--- frontend/scroll.js
+++ frontend/scroll.js
@@ -6,13 +6,23 @@ gsap
   .timeline({
     scrollTrigger: {
       trigger: container,
-      start: "top 100%",
-      end: "bottom 50%",
+      start: "top 80%",
+      end: "top 30%",
       toggleActions: "restart none none reverse",
-      scrub: true,
+      scrub: 1.5,
       markers: true,
     },
   })
   .to(container, {
-    scale: 2.0,
+    width: "100%",
+    scale: 1.9,
+    backgroundColor: "white",
+    duration: 0.5,
+    // height: "auto",
   });
+// gsap.to(".tt", {
+//   fontSize: "45px",
+//   scale: 0.8,
+//   duration: 1,
+//   transformOrigin: "center center",
+// });
